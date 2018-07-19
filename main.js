f1f5cf4b22fa20f47db3074fe769136b881295ab
Yifan Hong <jacky8hyf@gmail.com>
2018-07-18 23:38:36 -0700

    parse url params

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(function() {
-  
+    // parse params
+    var params = new URLSearchParams(window.location.search);
+    var width = parseInt(params.get("width")) || 10;
+    var height = parseInt(params.get("height")) || 10;
+    var wrap = ["true","1"].includes(params.get("wrap"));
+
 });
